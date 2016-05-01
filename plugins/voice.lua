27045f42ce246571e275d7d7627e7096133d8950
unknowntg <arashnomiri@gmail.com>
2016-05-01 19:02:43 +0430

    Create voice.lua

---
+++ plugins/voice.lua
@@ -0,0 +1,41 @@
+do
+
+local function run(msg, matches)
+
+
+local text = matches[1]
+
+  local b = 1
+
+  while b ~= 0 do
+    textc = text:trim()
+    text,b = text:gsub(' ','.') -- Fixing space problem ;)
+    
+    
+  if msg.to.type == 'user' then --Dont answer on private chat !
+      return nil
+      else
+  local url = "http://tts.baidu.com/text2audio?lan=en&ie=UTF-8&text="..textc
+  local receiver = get_receiver(msg)
+  local file = download_to_file(url,'text.ogg')
+      send_audio('chat#id'..msg.to.id, file, ok_cb , false)
+end
+end
+  end
+return {
+  description = "text to voice",
+  usage = {
+    "!voice [text]"
+  },
+  patterns = {
+    "^!voice +(.*)$",
+    "^/voice +(.*)$",
+    "^!tts +(.*)$",
+    "^/tts +(.*)$",
+    "^!ts +(.*)$",
+    "^/ts +(.*)$",
+  },
+  run = run
+}
+
+end
