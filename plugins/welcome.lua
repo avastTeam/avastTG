d89f94d2798435343ad6d022454ac769e1626c50
unknowntg <arashnomiri@gmail.com>
2016-05-01 22:12:20 +0430

    Update welcome.lua

--- plugins/welcome.lua
+++ plugins/welcome.lua
@@ -58,7 +58,7 @@ local function description_rules(msg, nama)
          rules = data[tostring(msg.to.id)]["rules"]
          rules = "\nGroup rules:\n________________\n"..rules.."\n"
       end
-      local sambutan = "Hi "..nama.."\nWelcome to'"..string.gsub(msg.to.print_name, "_", " ").."'\nGroup\nFor see commends list send /helps"
+      local sambutan = "Hi "..nama.."\nWelcome to'"..string.gsub(msg.to.print_name, "_", " ").."'\nGroup\nFor see commends list send helps"
       local text = sambutan..about..rules.."\n"
       local receiver = get_receiver(msg)
       send_large_msg(receiver, text, ok_cb, false)
