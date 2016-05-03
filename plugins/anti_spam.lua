1f9b84177ed3527967750b4d440c0fc2fbd34620
unknowntg <arashnomiri@gmail.com>
2016-05-03 11:51:40 +0430

    Update anti_spam.lua

--- plugins/anti_spam.lua
+++ plugins/anti_spam.lua
@@ -106,10 +106,10 @@ local function pre_process(msg)
           end
           local name = user_print_name(msg.from)
           --Send this to that chat
-          send_large_msg("chat#id"..msg.to.id, "User [ "..name.." ]"..msg.from.id.." Globally banned (spamming)")
+          send_large_msg("chat#id"..msg.to.id, "User [ "..name.." ]"..msg.from.id.." (بصورت جهانی بن شد (بخاطر اسپم )"
           local log_group = 1 --set log group caht id
           --send it to log group
-          send_large_msg("chat#id"..log_group, "User [ "..name.." ] ( @"..username.." )"..msg.from.id.." Globally banned from ( "..msg.to.print_name.." ) [ "..msg.to.id.." ] (spamming)")
+          send_large_msg("chat#id"..log_group, "User [ "..name.." ] ( @"..username.." )"..msg.from.id.." بصورت جهانی بن شد ( "..msg.to.print_name.." ) [ "..msg.to.id.." ] (spamming)")
         end
       end
       kicktable[user] = true
