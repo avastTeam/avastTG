941cf3d4c9a650dc323a97828b55cd9ebaac674e
unknowntg <arashnomiri@gmail.com>
2016-05-03 18:46:38 +0430

    Update lock_badword.lua

--- plugins/lock_badword.lua
+++ plugins/lock_badword.lua
@@ -14,7 +14,7 @@ if msg.to.type == 'chat' then
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
     if lock_adds == "yes" then
-        send_large_msg(chat, 'Badwords is not allowed here!!')
+        send_large_msg(chat, 'فحاشی در این گروه ممنوع است!!')
         chat_del_user(chat, user, ok_cb, true)
     end
 end
