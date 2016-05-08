64ec78bacd8c216b3a92d5d53e8f568a5907c40f
unknowntg <arashnomiri@gmail.com>
2016-05-08 17:59:45 +0430

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -3,7 +3,7 @@ do
 local function run(msg, matches)
   if matches[1] == "help" and matches[2] and matches[3] then
     if mamod(msg) then
-	    local file = "./root/unknowntg/data/help"..matches[2].."help.txt"..matches[3].."
+	    local file = "./root/unknowntg/data/help"..matches[2].."help.txt""..matches[3]..""
       local receiver = get_receiver(msg)
       send_document(receiver, file, ok_cb, false)
     end
