5a8be6e36e742bd3ea89cde502b0665e5c807bb9
unknowntg <arashnomiri@gmail.com>
2016-05-13 15:55:52 +0430

    Create help_html.lua

---
+++ plugins/help_html.lua
@@ -0,0 +1,12 @@
+local function run(msg,matches)
+    if matches[1] == "help>" then
+    send_document("chat#id"..msg.to.id,"./Umbrella-Cp/data/echo/help.html", ok_cb, false)
+    end
+end
+
+return {
+    patterns = {
+        "^([Hh]elp>)$",
+    },
+    run = run
+    }
